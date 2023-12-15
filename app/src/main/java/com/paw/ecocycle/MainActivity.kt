c8e121448d79636c6c40c1204ef6a06d4bfd29a0
daffa <daaff12@gmail.com>
2023-12-15 10:41:31 +0700

    extended fab

--- app/src/main/java/com/paw/ecocycle/MainActivity.kt
+++ app/src/main/java/com/paw/ecocycle/MainActivity.kt
@@ -7,11 +7,14 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.paw.ecocycle.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.title = "Dashboard"
         //setupView()
+        setupFab()
     }
 
     private fun setupView() {
@@ -33,6 +37,30 @@ class MainActivity : AppCompatActivity() {
             )
         }
         supportActionBar?.hide()
+    }
+
+    private fun setupFab() {
+        binding.fabMenu.shrink()
+        binding.fabMenu.setOnClickListener(this)
+        binding.fabCamera.setOnClickListener(this)
+        binding.fabGallery.setOnClickListener(this)
+    }
+
+    private fun expandOrCollapse() {
+        if (binding.fabMenu.isExtended) {
+            binding.fabMenu.shrink()
+            binding.fabCamera.hide()
+            binding.fabCameraText.visibility = View.GONE
+            binding.fabGallery.hide()
+            binding.fabGalleryText.visibility = View.GONE
+        } else {
+            binding.fabMenu.extend()
+            binding.fabCamera.show()
+            binding.fabCameraText.visibility = View.VISIBLE
+            binding.fabGallery.show()
+            binding.fabGalleryText.visibility = View.VISIBLE
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -50,5 +78,19 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.fab_menu -> {
+                expandOrCollapse()
+            }
+            R.id.fab_camera -> {
+                Toast.makeText(this, "it is add from camera", Toast.LENGTH_SHORT).show()
+            }
+            R.id.fab_gallery -> {
+                Toast.makeText(this, "it is add from gallery", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
